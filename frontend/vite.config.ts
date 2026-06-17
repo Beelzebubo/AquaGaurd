1945b2df0c4d3992f61feeba6ffa55374aa5c646
Beelzebubo <150050120+Beelzebubo@users.noreply.github.com>
2026-06-17 12:51:43 +0545

    commit

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,11 +2,11 @@ import { defineConfig } from 'vite';
 import { tanstackStart } from '@tanstack/react-start/plugin/vite';
 
 export default defineConfig({
-  plugins: [tanstackStart()], // Ensure the plugin is here!
+  plugins: [tanstackStart()],
   build: {
     rollupOptions: {
-      // Point Vite to your actual entry file (usually src/main.tsx or src/entry-client.tsx)
-      input: 'src/main.tsx', 
+     
+      input: 'start.ts',
       external: ['react-is'],
     },
   },
