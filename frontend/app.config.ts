912f954009ed000d03a0ceebb0b1e55bdfc94edc
Saumya Bikram Bartaula <bartaulasaumya@gmail.com>
2026-06-17 14:10:40 +0545

    Final cleanup: Fix app load error

--- frontend/app.config.ts
+++ frontend/app.config.ts
@@ -1,10 +1,7 @@
 import { defineConfig } from "@tanstack/start/config";
 
 export default defineConfig({
-    // Vinxi uses this to find your routes automatically based on your folder structure
-    routers: {
-        client: {
-            entry: "./index.tsx",
-        },
+    server: {
+        preset: "node",
     },
 });
